3766a414c3822132351b544d0321fa2f382b5ca3
David Obodo <obododavid5@gmail.com>
2021-11-09 16:23:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const elem = document.querySelectorAll("li")[1];
+
+        console.log(elem, "THE ELEMENTS");
+    }, []);
+    return (
+        <ul>
+            {[1, 2, 3, 4, 5, 6].map((item) => {
+                return <li>{item}</li>;
+            })}
+        </ul>
+    );
 }
 
 export default App;
